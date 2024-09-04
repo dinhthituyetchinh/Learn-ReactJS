27d5afe10c8aceeb29839e7dc97e84306801efd9
dinhthituyetchinh <fengdinh@gmail.com>
2024-09-04 21:52:41 +0700

    up 3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Navigate, Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
-
 
 
 function App() {
 
   return (
     <div className="App">
+
+      <Routes>
+      <Route path="/home" element={<Navigate to="/" />} />
+
+      {/* Redirect /post-list/:postId to /posts/:postId */}
+      <Route path="/post-list/:postId" element={<Navigate to="/posts/:postId" />} />
+      </Routes>
+
       <p>Header</p>
       <p>      <Link to='/todos'>Todo</Link>      </p>
       <p>      <Link to='/albums'>Album</Link>      </p>
-
+     
       <Outlet />
       <p>Footer</p>
     </div>
